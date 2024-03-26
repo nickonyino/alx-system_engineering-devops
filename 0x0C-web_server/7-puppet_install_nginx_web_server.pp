ae400e4882741034ecf5100b6b430dfdd7bdfcc4
nickonyino <bahatiluiz@gmail.com>
2024-03-26 11:43:26 -0700

    ssh

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,24 +1,47 @@
- Setup New Ubuntu server with nginx
-
-exec { 'update system':
-        command => '/usr/bin/apt-get update',
-}
-
+# Update system and install Nginx package
 package { 'nginx':
-	ensure => 'installed',
-	require => Exec['update system']
+  ensure => 'installed',
+  require => Exec['update_system'],
 }
 
-file {'/var/www/html/index.html':
-	content => 'Hello World!'
+exec { 'update_system':
+  command => '/usr/bin/apt-get update',
+  refreshonly => true,  # Only run when triggered
 }
 
-exec {'redirect_me':
-	command => 'sed -i "24i\	rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
-	provider => 'shell'
+# Ensure the index.html file exists
+file { '/var/www/html/index.html':
+  ensure  => 'file',
+  content => 'Hello World!',
 }
 
-service {'nginx':
-	ensure => running,
-	require => Package['nginx']
+# Use a template to manage Nginx configuration
+file { '/etc/nginx/sites-available/default':
+  ensure  => 'file',
+  content => template('module/nginx/default.conf.erb'),
+  notify  => Service['nginx'],  # Restart Nginx service when configuration changes
 }
+
+# Restart Nginx service
+service { 'nginx':
+  ensure => 'running',
+  enable => true,
+  require => Package['nginx'],
+}
+
+server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+
+    root /var/www/html;
+    index index.html;
+
+    server_name _;
+
+    location /redirect_me {
+        rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;
+    }
+
+    # Additional server configuration can go here
+}
+
