0cab24cf0b2e88e672418d1382488b0f0206dedd
nickonyino <bahatiluiz@gmail.com>
2024-05-14 12:21:43 -0700

    add

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,7 @@
+# Changes the OS configuration so that it is possible to login with the
+# user and open a file without any error message.
+
+exec {'OS security config':
+  command => 'sed -i "s/holberton/foo/" /etc/security/limits.conf',
+  path    => '/usr/bin/env/:/bin/:/usr/bin/:/usr/sbin/'
+}
